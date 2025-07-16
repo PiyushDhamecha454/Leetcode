952dd3492ca32c982560df38207b275f691a3b7d
Piyush Dhamecha <157054029+PiyushDhamecha454@users.noreply.github.com>
2025-07-16 11:10:34 +0530

    Time: 4 ms (61.36%) | Memory: 11 MB (40.16%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -7,10 +7,10 @@ public:
         for (int r = 0; r < s.size(); r++) {
             v[s[r] - 'A']++;
             mF = max(mF, v[s[r] - 'A']);
-            while (r - l + 1 - mF > k) {
+            /*while*/ if (r - l + 1 - mF > k) {
                 v[s[l] - 'A']--;
                 mF = 0;
-                for (int i = 0; i < 26; i++) mF = max(mF, v[i]);
+                // for (int i = 0; i < 26; i++) mF = max(mF, v[i]);
                 l++;
             }
             if (r - l + 1 - mF <= k) mL = max(mL, r - l + 1);

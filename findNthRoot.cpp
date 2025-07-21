42d2cf717fe629845ea2b6443e30d233ebae7ee1
PiyushDhamecha454 <piyushdhamecha327@gmail.com>
2025-07-21 20:14:54 +0530

    Find Nth Root (v2)

--- findNthRoot.cpp
+++ findNthRoot.cpp
@@ -1,12 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+long long power(int base, int exp, int limit) {
+    long long res = 1;
+    for (int i = 0; i < exp; i++) {
+        res *= base;
+        if (res > limit) return res;
+    }
+    return res;
+}
 int findNthRoot(int m, int n) {
     if (m == 1) return 1;
     int l = 1, r = m;
     while (l <= r) {
         int mid = l + (r - l) / 2;
-        if (pow(mid, n) == m) return mid;
-        else if (pow(mid, n) > m) r = mid - 1;
+        long long midPow = power(mid, n, m);
+        if (midPow == m) return mid;
+        else if (midPow > m) r = mid - 1;
         else l = mid + 1;
     }
     return -1;

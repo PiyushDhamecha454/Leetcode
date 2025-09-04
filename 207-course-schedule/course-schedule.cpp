380b42d94fbf5bc87df3a479e670b896b61f28b9
Piyush Dhamecha <157054029+PiyushDhamecha454@users.noreply.github.com>
2025-09-04 18:22:00 +0530

    Time: 8 ms (37.16%) | Memory: 20.2 MB (22.85%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,20 +1,23 @@
 class Solution {
-    bool dfs (int node, vector<bool>& visited, vector<bool>& onPath, vector<vector<int>>& adjList) {
-        if (onPath[node]) return true;
-        if (visited[node]) return false;
+    bool dfs(int node, vector<bool>& visited, vector<vector<int>>& adjList, vector<bool>& onPath) {
         visited[node] = true;
         onPath[node] = true;
-        for (int nei : adjList[node]) if (dfs(nei, visited, onPath, adjList)) return true;
+        for (int nei: adjList[node]) {
+            if (!visited[nei]) {
+                if (dfs(nei, visited, adjList, onPath)) return true;
+            }
+            else if (onPath[nei]) return true;
+        }
         onPath[node] = false;
         return false;
     }
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& p) {
-        int n = p.size();
-        vector<vector<int>> adjList(numCourses);
-        for (auto e : p) adjList[e[1]].push_back(e[0]);
-        vector<bool> visited(numCourses, false), onPath(numCourses, false);
-        for (int i = 0; i < numCourses; i++) if (!visited[i] && dfs(i, visited, onPath, adjList)) return false;
+    bool canFinish(int x, vector<vector<int>>& edges) {
+        vector<vector<int>> adjList(x + 1);
+        for (auto e : edges) adjList[e[1]].push_back(e[0]);
+        vector<bool> visited(x + 1, false);
+        vector<bool> onPath(x + 1, false);
+        for (int i = 0; i < x + 1; i++) if (!visited[i]) if (dfs(i, visited, adjList, onPath)) return false;
         return true;
     }
 };

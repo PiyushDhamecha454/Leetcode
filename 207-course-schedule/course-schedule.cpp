f94b09eb6c123d202c16e577501d49a4f8938817
Piyush Dhamecha <157054029+PiyushDhamecha454@users.noreply.github.com>
2025-09-04 18:26:26 +0530

    Time: 9 ms (29.56%) | Memory: 19.9 MB (50.70%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,23 +1,24 @@
 class Solution {
-    bool dfs(int node, vector<bool>& visited, vector<vector<int>>& adjList, vector<bool>& onPath) {
-        visited[node] = true;
-        onPath[node] = true;
-        for (int nei: adjList[node]) {
-            if (!visited[nei]) {
-                if (dfs(nei, visited, adjList, onPath)) return true;
-            }
-            else if (onPath[nei]) return true;
-        }
-        onPath[node] = false;
-        return false;
-    }
 public:
     bool canFinish(int x, vector<vector<int>>& edges) {
         vector<vector<int>> adjList(x + 1);
-        for (auto e : edges) adjList[e[1]].push_back(e[0]);
-        vector<bool> visited(x + 1, false);
-        vector<bool> onPath(x + 1, false);
-        for (int i = 0; i < x + 1; i++) if (!visited[i]) if (dfs(i, visited, adjList, onPath)) return false;
-        return true;
+        vector<int> indegree(x + 1, 0);
+        int c = 0;
+        queue<int> q;
+        for (auto e : edges) {
+            adjList[e[1]].push_back(e[0]);
+            indegree[e[0]]++;
+        }
+        for (int i = 0; i <= x; i++) if (!indegree[i]) q.push(i);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            c++;
+            for (int nei: adjList[node]) {
+                indegree[nei]--;
+                if (!indegree[nei]) q.push(nei);
+            }
+        }
+        return c == x + 1;
     }
 };
